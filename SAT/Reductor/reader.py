a45668f7ec542f7279213537a51ee6671c73b033
JuanDavid69 <juan.gomez.orozco@correounivalle.edu.co>
2020-09-24 19:20:29 -0500

    read_cnf

--- SAT/Reductor/reader.py
+++ SAT/Reductor/reader.py
@@ -1,6 +1,7 @@
 from os import system, getcwd, walk
 from os.path import join, sep
 import sys
+from pysat.formula import CNF
 
 def read_file():
     ALL_PROBLEMS = [] # Store all the SAT problems in format Problems->[Problem->[Clause->[variables]]]
@@ -31,4 +32,28 @@ def read_file():
     #print(ALL_PROBLEMS)
     return ALL_PROBLEMS
 
-#sread_file()
+def read_cnf_and_reduce():
+    ACTUAL_DIRECTORY = getcwd() # Get the current directory path (../SAT/Reductor)
+    PARENT_DIRECTORY = sep.join(ACTUAL_DIRECTORY.split(sep)[1:-1]) # Get the parent directory (../SAT)
+    PARENT_DIRECTORY = join(sep, PARENT_DIRECTORY) # Apeend SO separator to access the folder
+    SAT_instances_directory = join(PARENT_DIRECTORY, "TEST") # Joins the parent directory with InstanciasSAT to get into (../SAT/instanciasSAT)
+    X_SAT_directory = join(PARENT_DIRECTORY, "X-SAT") # Joins the parent directory with InstanciasSAT to get into (../SAT/X-SAT)
+    _, _, SAT_instances = next(walk(SAT_instances_directory))
+    print("REDUCIENDO INSTANCIAS A %s-SAT . . ." % x)
+    for SAT_instance in SAT_instances:
+        with open(join(SAT_instances_directory, SAT_instance), 'r') as fp:
+            sat_instance = CNF(from_fp=fp)
+            sat_instance_reduced = CNF()
+            
+            reduce(x, sat_instance, sat_instance_reduced)
+            for clause in sat_instance:
+                if int(x)
+            sat_instance_reduced.append([-1,2])
+            sat_instance_reduced.append([1,-3])
+            sat_instance_reduced.append([5,-4])
+            sat_instance_reduced.to_file(join(X_SAT_directory, SAT_instance[:-4] + '_reduced.cnf'))
+            print(sat_instance.clauses)
+            print(sat_instance_reduced.clauses)
+    print("INSTANCIAS REDUCIDAS A %s-SAT." % x)
+    
+read_cnf_and_reduce()
--- SAT/Reductor/reductor.py
+++ SAT/Reductor/reductor.py
@@ -1,12 +1,9 @@
 import sys
-import reader
 
-def reduce(x):
-    print("REDUCIENDO INSTANCIAS A %s-SAT . . ." % x)
-    instances = reader.read_file()
+def reduce(x,sat_instance, sat_instance_reduced):
     for instance in instances:
         problem_reduced = []
-        num_vars = instance[0][2]
+        num_vars = sat_instance.vars
         num_clauses= instance[0][3]
         for clause in instance:
             if clause[0] == 'p':
@@ -38,15 +35,52 @@ def reduce(x):
                     else:
                         problem_reduced.append(new_clause)
                         is_new_var_added = True
+            if int(x) < int(len(clause))
             
             instance[0][2] = num_vars
             instance[0][3] = str(len(problem_reduced) - 1)
 
         #print(instance[0])
-        for inst in problem_reduced:
-            print(str(inst) + "\n")
+        return sat_instance_reduced
+
+def increase(x,sat_instance, sat_instance_reduced):
+    
+    num_vars = sat_instance.nv
+    new_clause_size = int(x)
+
+    for clause in sat_instance:
+        clause_size = len(clause)
+
+        if new_clause_size == clause_size:
+            problem_reduced.append(clause)
+        else:
+            
+
+def read_cnf_and_reduce(x):
+    ACTUAL_DIRECTORY = getcwd() # Get the current directory path (../SAT/Reductor)
+    PARENT_DIRECTORY = sep.join(ACTUAL_DIRECTORY.split(sep)[1:-1]) # Get the parent directory (../SAT)
+    PARENT_DIRECTORY = join(sep, PARENT_DIRECTORY) # Apeend SO separator to access the folder
+    SAT_instances_directory = join(PARENT_DIRECTORY, "TEST") # Joins the parent directory with InstanciasSAT to get into (../SAT/instanciasSAT)
+    X_SAT_directory = join(PARENT_DIRECTORY, "X-SAT") # Joins the parent directory with InstanciasSAT to get into (../SAT/X-SAT)
+    _, _, SAT_instances = next(walk(SAT_instances_directory))
+    
+    print("REDUCIENDO INSTANCIAS A %s-SAT . . ." % x)
+    
+    for SAT_instance in SAT_instances:
+        with open(join(SAT_instances_directory, SAT_instance), 'r') as fp:
+            sat_instance = CNF(from_fp=fp)
+            sat_instance_reduced = CNF()
+            
+            sat_instance_reduced = reduce(x, sat_instance, sat_instance_reduced)
+            
+            sat_instance_reduced.to_file(join(X_SAT_directory, SAT_instance[:-4] + '_reduced.cnf'))
+            print(sat_instance.clauses)
+            print(sat_instance_reduced.clauses)
+    
     print("INSTANCIAS REDUCIDAS A %s-SAT." % x)
+    
+read_cnf_and_reduce()
 
 x = str(sys.argv[1])
 
-reduce(x)
+read_cnf_and_reduce(x)
